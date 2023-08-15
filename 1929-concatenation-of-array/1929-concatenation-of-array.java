ed1acba30627c7f097ab0f509141fbb05f20658c
Shivam Ranpise <99407071+Shivam-Ranpise@users.noreply.github.com>
2023-08-15 20:52:04 +0530

    Time: 1 ms (90.88%), Space: 44.3 MB (79.54%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,20 +1,40 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-        // int n = 2 * nums.length;
-        // int [] ans = new int[n];
-        // for(int i = 0; i < nums.length; i++){
-        //     ans[i] = nums[i];
-        // }
-        // int index = nums.length;
-        // for(int i = 0; i < nums.length; i++){
-        //     ans[index] = nums[i];
-        //     index++;
-        // }
-        // return ans;
+        /* brutforce approch..........
+        
+        
+        int n = 2 * nums.length;
+        int [] ans = new int[n];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+        }
+        int index = nums.length;
+        for(int i = 0; i < nums.length; i++){
+            ans[index] = nums[i];
+            index++;
+        }
+        return ans;
+        
+        */
+        
+        /* first optimized approch....... (1 ms 90.88% faster & memory 44.1 MB 88.30%  less)
+        
         int n = 2 * nums.length;
         int [] ans = new int[n];
         for(int i = 0; i < n; i++){
             ans[i] = nums[i % nums.length];
+        }
+        return ans;
+        
+        */
+        
+        //second optimized approch.....
+        
+        int n = nums.length;
+        int [] ans = new int[2 * n];
+        for(int i = 0; i < n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
         }
         return ans;
     }
