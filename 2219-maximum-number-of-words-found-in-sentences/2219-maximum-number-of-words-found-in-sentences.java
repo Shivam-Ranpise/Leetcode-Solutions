7677f03858c50d0917f5fa41de196c9c6f91e6a5
Shivam Ranpise <99407071+Shivam-Ranpise@users.noreply.github.com>
2023-09-24 15:25:30 +0530

    Time: 2 ms (96.9%), Space: 42 MB (67.51%) - LeetHub

--- 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
+++ 2219-maximum-number-of-words-found-in-sentences/2219-maximum-number-of-words-found-in-sentences.java
@@ -6,5 +6,30 @@ class Solution {
             count = Math.max(count, new StringTokenizer(s).countTokens());
         }
         return count;
+
+        /*
+
+        int max = 0;
+        for (String s : sentences) {
+            int x = s.indexOf(" "), count = 1;
+            while (x != -1) {
+                count++;
+                x = s.indexOf(" ", x + 1);
+            }
+            max = Math.max(count, max);
+        }
+        return max; 
+
+        */
+        /*
+
+        int max = 1;
+        for(int i = 0; i < sentences.length; i++){
+            String[] arr = sentences[i].split(" ");
+            max = Math.max(max,arr.length);
+        }
+        return max; 
+
+        */
     }
 }
